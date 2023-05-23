fc087591797dabf06b52b459e2addce3b839e89c
Terence Rajapaksage <terenceraja@hotmail.fr>
2023-05-23 22:42:24 +0200

    fixed offers route

--- routes/offers.js
+++ routes/offers.js
@@ -7,8 +7,8 @@ const { checkBody } = require("../modules/checkBody");
 
 router.get("/:projectId", (req, res) => {
   Offer.find({ project: req.params.projectId })
-    // .populate("project")
-    // .populate("user_Id")
+    .populate("project")
+    .populate("user_Id")
     .then((projectData) => {
       res.json({ projectData: projectData });
     });
