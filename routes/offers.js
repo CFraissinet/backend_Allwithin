e34e22d0373ec5a1e062fd422883dd8ebbc435ca
Benoit <giannini.benoit@gmail.comwd>
2023-05-25 09:12:17 +0200

    add routes for refuse man from offers

--- routes/offers.js
+++ routes/offers.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 require("../models/connection");
 const Offer = require("../models/offers");
+const Project = require("../models/projects");
+const User = require("../models/users");
 const { checkBody } = require("../modules/checkBody");
 
 router.get("/:projectId", (req, res) => {
@@ -13,5 +15,37 @@ router.get("/:projectId", (req, res) => {
       res.json({ projectData: projectData });
     });
 });
+
+// router.put("/refuse", (req, res) => {
+//   Offer.findOne({ project: req.body.project }).then((data) => {
+//     const newIds = data.user_Id.filter(
+//       (user_Id) => user_Id.toString() !== req.body.user_Id
+//     );
+//     console.log(newIds);
+//     Offer.updateOne({ _id: data._id }, { user_Id: newIds });
+//     console.log(data.user_Id);
+//   });
+// });
+
+router.put("/refuse", (req, res) => {
+  Offer.findById(req.body._id).then((data) => {
+    const newIds = data.user_Id.filter(
+      (user_Id) => user_Id.toString() !== req.body.user_Id
+    );
+    console.log(data.user_Id);
+    Offer.updateOne({ _id: data._id }, { user_Id: newIds }).then((data) => {
+      res.json({ result: data.user_id });
+    });
+  });
+});
+
+// router.post("", (req, res) => {
+//   Offer.find({ project: req.params.projectId })
+//     // .populate("project")
+//     .populate("user_Id")
+//     .then((projectData) => {
+//       res.json({ projectData: projectData });
+//     });
+// });
 
 module.exports = router;
